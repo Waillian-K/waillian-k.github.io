294f5837f96fa3fe998062bdd1e5face39c0da35
waillian-K <waillianchit@gmail.com>
2025-03-03 20:13:17 +0630

    project

---
+++ JAVA_SCRIPT/custom.js
@@ -0,0 +1,20 @@
+// var name="Waillian";  // var is a global variable
+// document.write(name);
+// document.write("<br>");
+// var name="Waillian Update" // Update the value of name variable and can be re-declared
+// document.write(name);
+// document.write("<br>");
+
+// let age=25;  // let is a local variable
+// document.write(age);
+// document.write("<br>");
+// age=26;  // Update the value of age variable but can't be re-declared like var variable 
+// document.write(age);
+// document.write("<br>");
+
+// const pi=3.1416;  // const is a constant variable
+// document.write(pi);
+// document.write("<br>");
+// // pi=3.1417;  // Error because const variable can't be updated
+// // document.write(pi);
+// // document.write("<br>");
